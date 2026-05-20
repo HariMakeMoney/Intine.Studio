e9aefae9b81af5e9228fd47d852fab97e8b09fbf
Hari <harigod2505@gmail.com>
2026-05-20 17:37:00 +0530

    Minor Tweaks

--- js/main.js
+++ js/main.js
@@ -239,3 +239,13 @@ if (form) {
   });
 }
 
+// BorderGlow Interaction Logic
+document.querySelectorAll('.border-glow').forEach(el => {
+  el.addEventListener('mousemove', e => {
+    const rect = el.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    el.style.setProperty('--mouse-x', `${x}px`);
+    el.style.setProperty('--mouse-y', `${y}px`);
+  });
+});
